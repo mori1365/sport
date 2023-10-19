66d001e132b10bb49ff70fffe69e6bb8391d8594
mori1365 <105076694+mori1365@users.noreply.github.com>
2023-10-19 14:39:50 +0330

    new

---
+++ app.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById("menu");
+const navbar = document.querySelector(".navbar");
+const clos = document.querySelector(".close");
+menu.onclick = () =>{
+    menu.style.display = "none";
+    navbar.style.top = "10%";
+    clos.style.display = "block";
+}
+clos.onclick = () =>{
+    menu.style.display = "block";
+    navbar.style.top = "-100%";
+    clos.style.display = "none";
+}
+window.onscroll = () =>{
+    menu.style.display = "block";
+    navbar.style.top = "-100%";
+    clos.style.display = "none";
+}
